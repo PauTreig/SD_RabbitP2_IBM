9c35f42eb00230ed20cb3879287ba2875c811b37
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2019-05-21 18:02:30 +0200

    Main with bad comunication
    In addition, I added the rules ro make the program usable. We will add the rules in the readme after finishing the practice

--- __main__.py
+++ __main__.py
@@ -1,50 +1,57 @@
-import random, sys, pika, json, os
+import random
 import pywren_ibm_cloud as pywren
+import sys
+import pika 
 
-def connection():
-	connection = pika.BlockingConnection(pika.URLParameters(url))
+def my_master_function(x):
+	pywren_Conf =  json.loads(os.environ.get('PYWREN_CONFIG', ''))
+	url = pywren_Conf['rabbitmq']['amqp_url']
+	params = pika.URLParameters(url)
+	connection = pika.BlockingConnection(params)
 	channel = connection.channel()
-	return channel
-
-def callback(ch, method, properties, body):
-    pw.map
-
-def my_function_master(nfunctions):
-	listrand[]	
-	channel = connection()
-	channel.queue_declare(queue='master_queue')
-	i=0
-	channel.basic_consume(callback, queue='master_queue', auto_ack=True)
-	channel.start_consuming()
-	while(i < nfunctions):
-		exchange, queue, body = channel.basic_get(exchange='', queue='master_queue', body=str(body))
-		
-		if(body!='None'):
+	channel.queue_declare(queue = 'master')
+	i=1
+	while i<x+1:
+		#codi llegir missatges
+		method_frame, header_frame, body = channel.basic_get('master')
+		if body != None:
+			#codi enviar missatges
+			taula = body.decode('utf-8')
+			ran=random.randint(0,1000)
+			channel.basic_publish(exchange='',routing_key=taula,body=str(ran))
+			return body.decode('utf-8')
 			i+=1
-			listrand.append((int(body))
-
-	while(len(listrand)>0):
-		randnum=random.randint(0,len(listrand))
-		channel.basic_publish(exchange='', routing_key=str(randnum),body='fet')
-		listrand.delete(listrand[randnum])
-	channel.close()
-
-
+			
 def my_map_function(x):
-	listrand[]
+	pywren_Conf =  json.loads(os.environ.get('PYWREN_CONFIG', ''))
+	url = pywren_Conf['rabbitmq']['amqp_url']
+	params = pika.URLParameters(url)
+	connection = pika.BlockingConnection(params)
+	channel = connection.channel()
+	channel.queue_declare(queue = str(x))
+	#codi enviar missatges
+	i=0
+	channel.basic_publish(exchange='',routing_key='master',body=str(x))
+	while i<1:
+		method_frame, header_frame, body = channel.basic_get(str(x))
+		if body != None:
+			i+=1
+		else:
+			print('No message returned')
+	return int(body.decode('utf-8'))
+	#codi llegir missatges
 	
-	num=random.randint(0,1001)
-	channel = connection()
-	channel.queue_declare(queue=str(x))
-	channel.basic_publish(exchange='', routing_key='master_queue', body=str(x))
-	channel.close()
 	
-nfunctions=int(sys.argv[1])
-pw_config = json.loads(os.environ.get('PYWREN_CONFIG', ''))
-url=pw_config['rabbitmq']['amqp_url']
-pw = pywren.ibm_cf_executor(rabbitmq_monitor=True)
-pw.call_async(my_function_master, nfunctions)
-pw = pywren.ibm_cf_executor(rabbitmq_monitor=True)
-pw.map(my_map_function, range(nfunctions))
-print(pw.get_result())
+	
+	#return(random.randint(0,1001))
 
+if len(sys.argv)==2 :
+	nMaps=int(sys.argv[1])  #number of maps 
+	idlist= list(range(1,nMaps+1)) #a list of the id of each map
+	pw = pywren.ibm_cf_executor(rabbitmq_monitor=True)
+	pw.call_async(my_master_function,nMaps)
+	pw2 = pywren.ibm_cf_executor(rabbitmq_monitor=True)
+	pw2.map(my_map_function, idlist)
+	print(pw2.get_result())
+else:
+	print('You have to pass the number of maps that you want to create')
