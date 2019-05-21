488007392ea53e7ca6c9f47bc818de6ec96be124
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2019-05-21 19:01:28 +0200

    master is done, dunno if it works

--- __main__.py
+++ __main__.py
@@ -10,17 +10,28 @@ def my_master_function(x):
 	connection = pika.BlockingConnection(params)
 	channel = connection.channel()
 	channel.queue_declare(queue = 'master')
+	listrand = []
 	i=1
 	while i<x+1:
-		#codi llegir missatges
-		method_frame, header_frame, body = channel.basic_get('master')
+		method_frame, properties, body = channel.basic_get('master')
 		if body != None:
-			#codi enviar missatges
-			taula = body.decode('utf-8')
-			ran=random.randint(0,1000)
-			channel.basic_publish(exchange='',routing_key=taula,body=str(ran))
-			return body.decode('utf-8')
-			i+=1
+			channel.basic_ack(method_frame.delivery_tag)
+			listrand.append(int(body.decode('utf-8')))
+			i=i+1	
+	i=1
+	while i<x+1:
+		channel.queue_declare(queue = str(i))
+		channel.basic_publish(exchange='',routing_key=str(i),body=str(x))
+		
+	while len(listrand) > 0:
+		index = randint(0, len(listrand) - 1)
+		listActive = listrand.pop(index)
+		channel.queue_declare(queue = str(index+1))
+		channel.basic_publish(exchange='',routing_key=str(index+1),body='Active')
+		
+	channel.close()
+	connection.close()
+	return listrand
 			
 def my_map_function(x):
 	pywren_Conf =  json.loads(os.environ.get('PYWREN_CONFIG', ''))
@@ -38,12 +49,10 @@ def my_map_function(x):
 			i+=1
 		else:
 			print('No message returned')
+
+	channel.close()
+	connection.close()
 	return int(body.decode('utf-8'))
-	#codi llegir missatges
-	
-	
-	
-	#return(random.randint(0,1001))
 
 if len(sys.argv)==2 :
 	nMaps=int(sys.argv[1])  #number of maps 
@@ -52,6 +61,6 @@ if len(sys.argv)==2 :
 	pw.call_async(my_master_function,nMaps)
 	pw2 = pywren.ibm_cf_executor(rabbitmq_monitor=True)
 	pw2.map(my_map_function, idlist)
-	print(pw2.get_result())
+	print(pw.get_result())
 else:
 	print('You have to pass the number of maps that you want to create')
